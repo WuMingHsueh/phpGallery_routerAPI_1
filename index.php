102ee5bd4b209a6cf74cfe9f34a5f43b631cecd5
Wu Ming-Hsueh <ri3567@gmail.com>
2018-06-17 01:24:23 +0800

    修正 authorization Unfine index

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ $pathInfoData = explode('/', $pathInfo);
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-$className  = strtoupper(substr($pathInfoData[0], 0, 1)) . substr($pathInfoData[0], 1) . 'Controller';
+$className = strtoupper(substr($pathInfoData[0], 0, 1)) . substr($pathInfoData[0], 1) . 'Controller';
 $contentType = explode(';', $_SERVER['CONTENT_TYPE'])[0];
-$authorization = $_SERVER['HTTP_AUTHORIZATION'];
+$authorization = $_SERVER['HTTP_AUTHORIZATION'] ?? null;
 if (file_exists('./src/controllers/' . $className . '.php')) {
-    $class = 'GalleryAPI\\controllers\\' . $className ;
+    $class = 'GalleryAPI\\controllers\\' . $className;
     $provider = new $class($pathInfoData, $method, $contentType, $authorization);
 }
