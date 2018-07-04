c67e06ce0e0beb19a92ef19a4993eb1316143a22
Wu Ming-Hsueh <ri3567@gmail.com>
2018-07-04 12:01:59 +0800

    完成封面出圖

--- src/service/CoverService.php
+++ src/service/CoverService.php
@@ -9,6 +9,8 @@ class CoverService
     private $coverSouceFolder;
     private $coverFolder;
     private $coverFile;
+    private $coverExtensionFix = 'jpg';
+    private $coverFirstSize = ['width' => 90, 'height' => 90];
     
     private $albumId;
     private $fileName;
@@ -16,9 +18,9 @@ class CoverService
     public function __construct($albumId)
     {
         $this->coverSouceFolder = dirname($_SERVER['DOCUMENT_ROOT']) . "/phpWarehouse/" . Environment::PROJECT_NAME . "/upload/";
-        $this->coverFolder = $_SERVER['DOCUMENT_ROOT'] . "/" . Environment::PROJECT_NAME . "/cover/";
+        $this->coverFolder = $_SERVER['DOCUMENT_ROOT'] . "/" . Environment::PROJECT_NAME . "/image/cover/";
         $this->albumId = $albumId;
-        $this->fileName = $albumId . '.jpg';
+        $this->fileName = "{$albumId}.{$this->coverExtensionFix}";
         $this->coverFile = $this->coverFolder . $this->fileName;
     }
 
@@ -30,16 +32,56 @@ class CoverService
 
     public function generateCoverLevel1(array $fileNameData)
     {
-        print_r($fileNameData);
+        $firstImage = Image::make($this->coverSouceFolder . $fileNameData[0]);
+        $firstImage->resize(
+            $this->coverFirstSize['width'],
+            $this->coverFirstSize['height']
+        )->save(
+            $this->coverFile
+        );
     }
 
     public function generateCoverLevel2(array $fileNameData)
     {
-        print_r($fileNameData);
+        $cover = Image::make($this->coverFile);
+        $first = Image::make($this->coverSouceFolder . $fileNameData[0]);
+        $second = Image::make($this->coverSouceFolder . $fileNameData[1]);
+        $cover->resize(
+            $this->coverFirstSize['width'],
+            $this->coverFirstSize['height']
+        )->insert(
+            $first->resize(
+                $this->coverFirstSize['width'] / 2,
+                $this->coverFirstSize['height']
+            ),
+            'left'
+        )->insert(
+            $second->resize(
+                $this->coverFirstSize['width'] / 2,
+                $this->coverFirstSize['height']
+            ),
+            'right'
+        )->save();
     }
 
     public function generateCoverLevel3(array $fileNameData)
     {
-        print_r($fileNameData);
+        $this->generateCoverLevel1($fileNameData);
+        $cover = Image::make($this->coverFile);
+        $second = Image::make($this->coverSouceFolder . $fileNameData[1]);
+        $third = Image::make($this->coverSouceFolder . $fileNameData[2]);
+        $cover->insert(
+            $second->resize(
+                $this->coverFirstSize['width'] / 2,
+                $this->coverFirstSize['height'] / 2
+            ),
+            'bottom-left'
+        )->insert(
+            $third->resize(
+                $this->coverFirstSize['width'] / 2,
+                $this->coverFirstSize['height'] / 2
+            ),
+            'bottom-right'
+        )->save();
     }
 }
